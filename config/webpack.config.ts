ee27e9497a28809e3a61c54d43cf4471400a672d
keli <15302737418@163.com>
2024-12-12 11:56:13 +0800

    fix: 修改构建目录

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -13,7 +13,7 @@ export const initWebpackConfig = (mode: "development" | "production"):Configurat
     entry: path.resolve(__dirname, '../src/index.tsx'), // 文件的入口
     output: {
       filename: isPro ? 'js/[name].[chunkhash:8].js' : '[name].[hash:8].js', // 文件名
-      path: path.resolve(__dirname, '../dist') // 文件输出地址
+      path: path.resolve(__dirname, '../build') // 文件输出地址
     },
     module: {
       rules: [
