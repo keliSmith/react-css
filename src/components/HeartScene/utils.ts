290680188287b3816c210a565e91b10998b77b54
keli <15302737418@163.com>
2024-12-30 10:33:34 +0800

    fix: 文案修改调整

--- src/components/HeartScene/utils.ts
+++ src/components/HeartScene/utils.ts
@@ -384,7 +384,7 @@ export function showTime() {
     minutes = '0' + minutes;
   if (seconds < 10)
     seconds = '0' + seconds;
-  const currentTimeString = 'Dear Yue:<br>'
+  const currentTimeString = 'Dear ZhuZhu:<br>'
 		+ 'I have been in love with you for:<br> '
 		+ '&nbsp&nbsp&nbsp&nbsp&nbsp'
 		+ '<c style="color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px" >' + days +' </c>day '
@@ -393,7 +393,7 @@ export function showTime() {
 		+'<c style="color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px" >' +seconds+' </c>sec<br>'
 		+ '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp只愿执子之手,与子偕老.<br>'
 		+ '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'
-		+	'<c style="color: #CCCCCC; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:20px" >----王八婆   2024-10-08</c>';
+		+	'<c style="color: #CCCCCC; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:20px" >----王八婆   2024-12-30</c>';
   const showTimeHtml = document.getElementById('show') as HTMLElement;
   showTimeHtml ? showTimeHtml.innerHTML=currentTimeString : '';  // 改这地方
 }
